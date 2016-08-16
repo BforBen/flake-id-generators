1c484a3d6b4ed9ebed19a47fc023e16e39d35f42
Ben <BforBen@users.noreply.github.com>
2016-08-16 14:29:42 +0100

    Bump to .NET v4.5

---
+++ FlakeGen/Instance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FlakeGen;
+
+namespace Flake
+{
+    public sealed class IdGen
+    {
+        private static volatile Id64Generator FlakeGen;
+        private static object syncRoot = new Object();
+
+        private IdGen() { }
+
+        public static Id64Generator Instance
+        {
+            get
+            {
+                if (FlakeGen == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (FlakeGen == null)
+                        {
+                            FlakeGen = new Id64Generator(generatorId: 0);
+                        }
+                    }
+                }
+
+                return FlakeGen;
+            }
+        }
+    }
+}
